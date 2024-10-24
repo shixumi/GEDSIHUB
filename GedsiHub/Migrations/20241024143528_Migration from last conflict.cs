f0c5247467ccbb61e649f3101014b55892ef515f
Markerist <markangelfesguerra@iskolarngbayan.pup.edu.ph>
2024-10-24 22:37:10 +0800

    Fixed conflict

---
+++ GedsiHub/Migrations/20241024143528_Migration from last conflict.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GedsiHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrationfromlastconflict : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 14, 35, 27, 713, DateTimeKind.Utc).AddTicks(528));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 14, 35, 27, 713, DateTimeKind.Utc).AddTicks(530));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 14, 35, 27, 713, DateTimeKind.Utc).AddTicks(532));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 14, 35, 27, 713, DateTimeKind.Utc).AddTicks(533));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 14, 35, 27, 713, DateTimeKind.Utc).AddTicks(535));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 14, 35, 27, 713, DateTimeKind.Utc).AddTicks(537));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 14, 35, 27, 713, DateTimeKind.Utc).AddTicks(538));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 13, 53, 4, 945, DateTimeKind.Utc).AddTicks(4771));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 13, 53, 4, 945, DateTimeKind.Utc).AddTicks(4773));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 13, 53, 4, 945, DateTimeKind.Utc).AddTicks(4774));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 13, 53, 4, 945, DateTimeKind.Utc).AddTicks(4776));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 13, 53, 4, 945, DateTimeKind.Utc).AddTicks(4778));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 6,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 13, 53, 4, 945, DateTimeKind.Utc).AddTicks(4779));
+
+            migrationBuilder.UpdateData(
+                table: "Modules",
+                keyColumn: "ModuleId",
+                keyValue: 7,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 24, 13, 53, 4, 945, DateTimeKind.Utc).AddTicks(4781));
+        }
+    }
+}
