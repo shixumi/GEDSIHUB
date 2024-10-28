c8cee3e7278848e2f9b42e891f3c082b0068beb0
Alexander Porlares <92439887+Rednaxela5@users.noreply.github.com>
2024-10-29 04:08:03 +0800

    Enhance user dashboard and feedback management
    * feedback.css:
      * Added styles for "Mark as Resolved" button.
    
    * ProfileViewMoreFunction.js:
      * Added function to toggle visibility of additional user profile details.

---
+++ GedsiHub/wwwroot/js/ProfileViewMoreFunction.js
@@ -0,0 +1,8 @@
+﻿document.getElementById('viewMoreButton').addEventListener('click', function () {
+    const additionalDetails = document.querySelector('.additional-details');
+    additionalDetails.classList.toggle('show'); // Toggle visibility
+
+    // Change the button text based on visibility
+    const spanElement = this.querySelector('span');
+    spanElement.innerHTML = additionalDetails.classList.contains('show') ? '< Back' : 'View More >';
+});
