0a9446e2a8dabde3a790b60bca0171817e8592e1
Alexander Porlares <92439887+Rednaxela5@users.noreply.github.com>
2024-11-01 02:23:15 +0800

    Implemented H5P Content!!!

---
+++ GedsiHub/Controllers/DataController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class DataController : Controller
+{
+    private readonly WatershedApiService _apiService;
+
+    public DataController(WatershedApiService apiService)
+    {
+        _apiService = apiService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        try
+        {
+            string xAPIData = await _apiService.GetXAPIDataAsync();
+            // Process the xAPI data or pass it to the view
+            return View("Index", xAPIData);
+        }
+        catch (Exception ex)
+        {
+            // Handle errors
+            return View("Error", ex.Message);
+        }
+    }
+}
