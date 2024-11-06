87cf930147e3e7621bc981f0c57077e78fe53f9a
Alexander Porlares <92439887+Rednaxela5@users.noreply.github.com>
2024-11-06 09:24:19 +0800

    Enhanced the spinner - spinner can now be seen even after a modal

--- GedsiHub/wwwroot/js/spinner.js
+++ GedsiHub/wwwroot/js/spinner.js
@@ -21,33 +21,8 @@ function hideSpinner() {
     document.getElementById("spinner").style.display = "none";
 }
 
-function submitFormWithSpinner() {
-    // Hide other modal content
-    $('.modal-body').children().not('#spinner-modal').hide();
-    // Show the spinner
-    $('#spinner-modal').show();
-}
-
-// Show the spinner when an AJAX request starts
-$(document).ajaxStart(function () {
-    $("#globalSpinner").show();
-});
-
-// Hide the spinner when an AJAX request completes
-$(document).ajaxStop(function () {
-    $("#globalSpinner").hide();
-});
-
-// Optionally, for full page transitions (non-AJAX)
-$(document).ready(function () {
-    // Show the spinner before unloading the page
-    $(window).on('beforeunload', function () {
-        $("#globalSpinner").show();
-    });
-});
 
 
-// Hide the spinner after 10 seconds (as a fallback)
-setTimeout(function () {
-    $("#globalSpinner").hide();
-}, 10000); // 10,000 milliseconds = 10 seconds
+$(window).on('load', function () {
+    $('#spinner').hide();
+}) 
