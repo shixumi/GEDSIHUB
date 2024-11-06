8580a0aa32f6ed9284da48e0175fc4e8c39c136c
Markerist <markangelfesguerra@iskolarngbayan.pup.edu.ph>
2024-11-06 13:38:16 +0800

    Removed conflicts

--- GedsiHub/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ GedsiHub/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
 using System.Text;
-using Microsoft.AspNetCore.Authorization;
 
 [AllowAnonymous]
 public class ConfirmEmailModel : PageModel
