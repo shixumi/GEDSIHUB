d6003eee37fb1e9ddc4abcd22726c7df0af009fb
Alexander Porlares <92439887+Rednaxela5@users.noreply.github.com>
2024-11-07 03:55:58 +0800

    Updated Profile Picture for Forums

--- GedsiHub/ViewComponents/ProfilePictureViewComponent.cs
+++ GedsiHub/ViewComponents/ProfilePictureViewComponent.cs
@@ -14,16 +14,27 @@ namespace GedsiHub.ViewComponents
             _userManager = userManager;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(string userId = null)
         {
+            // Default image path if no profile picture is set
             string profilePicturePath = Url.Content("~/images/User.png");
 
-            if (User.Identity.IsAuthenticated)
+            // If a userId is provided, get the specified user's profile picture
+            if (!string.IsNullOrEmpty(userId))
             {
-                var user = await _userManager.GetUserAsync(HttpContext.User);
+                var user = await _userManager.FindByIdAsync(userId);
                 if (user != null && !string.IsNullOrEmpty(user.ProfilePicturePath))
                 {
                     profilePicturePath = user.ProfilePicturePath;
+                }
+            }
+            // If no userId is provided, use the current logged-in user's profile picture
+            else if (User.Identity.IsAuthenticated)
+            {
+                var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+                if (currentUser != null && !string.IsNullOrEmpty(currentUser.ProfilePicturePath))
+                {
+                    profilePicturePath = currentUser.ProfilePicturePath;
                 }
             }
 
